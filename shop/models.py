ecc6c0bc709c06a9e15895140ce0c3026d4eb7df
Schtirlitz80 <Schtirlitz80@mail.ru>
2023-02-22 23:16:43 +0300

    Minor changes

--- shop/models.py
+++ shop/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.CharField(max_length=200, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
     class Meta:
         ordering = ('name',)
@@ -26,6 +30,9 @@ class Product(models.Model):
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
 
     class Meta:
         ordering = ('name',)
