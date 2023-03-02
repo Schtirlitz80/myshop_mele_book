33641e4ed5709f1fb06652c72a0f41a08869c4c0
Schtirlitz80 <Schtirlitz80@mail.ru>
2023-03-02 23:49:21 +0300

    pdf order detail (bill) in orders admin

--- orders/admin.py
+++ orders/admin.py
@@ -36,6 +36,15 @@ def export_to_csv(modeladmin, request, queryset):
 export_to_csv.short_description = 'Export to CSV'
 
 
+def order_pdf(obj):
+    return mark_safe('<a href="{}">PDF</a>'.format(
+        reverse('shop:orders:admin_order_pdf', args=[obj.id])
+    ))
+
+
+order_pdf.short_description = 'Invoice'
+
+
 # Register your models here.
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
@@ -46,9 +55,8 @@ class OrderItemInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                         'address', 'postal_code', 'city', 'paid',
-                        'created', 'updated', order_detail]
+                        'created', 'updated', order_detail, order_pdf]
 
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
-
