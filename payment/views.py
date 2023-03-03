b216c0655f03c11d3ca39a2545db8f3c97bb61d8
Schtirlitz80 <Schtirlitz80@mail.ru>
2023-03-03 11:06:49 +0300

    Pdf order is sending by email now

--- payment/views.py
+++ payment/views.py
@@ -2,6 +2,12 @@ import braintree
 from django.shortcuts import render, redirect, get_object_or_404
 from orders.models import Order
 
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
+import weasyprint
+from io import BytesIO
+
 
 # Create your views here.
 def payment_process(request):
@@ -25,6 +31,25 @@ def payment_process(request):
             # Сохранение ID транзакции в заказе.
             order.braintree_id = result.transaction.id
             order.save()
+            # Создание электронного сообщения.
+            subject = 'My shop - Invoice no. {}'.format(order.id)
+            message = 'Please, find attached the invoice for your recent purchase.'
+            email = EmailMessage(subject,
+                                 message,
+                                 'admin@myshop.com',
+                                 [order.email])
+            # Формирование PDF.
+            html = render_to_string('orders/order/pdf.html', {'order': order})
+            out = BytesIO()
+            stylesheets = [weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
+            weasyprint.HTML(string=html).write_pdf(out, stylesheets=stylesheets)
+            # Прикрепляем PDF к элетронному сообщению.
+            email.attach('order_{}.pdf'.format(order_id),
+                         out.getvalue(),
+                         'application/pdf')
+            # Отправка сообщения.
+            email.send()
+
             return redirect('payment:done')
         else:
             return redirect('payment:canceled')
